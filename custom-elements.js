24423a22f555012f93902aeabba40440276b6dad
Yvonne Yip <ykyyip@google.com>
2013-05-10 11:30:59 -0700

    move inline script to file

--- custom-elements.js
+++ custom-elements.js
@@ -36,6 +36,7 @@ if (!window.Loader) {
   var path = basePath + 'tools/loader/loader.js';
   document.write('<script src="' + path + '"></script>');
 }
-document.write('<script>Loader.load("' + scopeName + '")</script>');
+//document.write('<script>Loader.load("' + scopeName + '")</script>');
+document.write('<script src="' + basePath + 'run-loader.js"></script>');
 
 })();
